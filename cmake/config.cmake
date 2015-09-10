c466c2c0022c5030c6d1f8706824aa70c3a260ac
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-09-10 11:12:42 -0400

    Add compiler configuration logic
    Add a bunch of logic to set up the compiler configuration the way we
    want/need. In particular, this turns on C++11 support and checks for
    some required language features, and also turns on a whole bunch of
    warnings that we would like enabled by default, some of which are
    promoted to errors.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,4 +1,99 @@
+include(CheckCXXCompilerFlag)
+
+#------------------------------------------------------------------------------
+# Execute a try_compile test with verbose status
+function(qte_verbose_try_compile variable file message)
+  message(STATUS "Checking if ${message}...")
+  try_compile(${variable}
+    ${CMAKE_CURRENT_BINARY_DIR}
+    ${CMAKE_CURRENT_LIST_DIR}/configcheck/${file}
+    CMAKE_FLAGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}"
+  )
+  if(${variable})
+    message(STATUS "Checking if ${message} - yes")
+  else()
+    message(STATUS "Checking if ${message} - no")
+  endif()
+endfunction()
+
+#------------------------------------------------------------------------------
+# Add first of specified flags that is supported by the compiler
+function(qte_add_cxx_flags_priority)
+  string(REPLACE " " ";" initial_flags ${CMAKE_CXX_FLAGS})
+  foreach(flag ${ARGN})
+    list(FIND initial_flags ${flag} FLAG_INDEX)
+    if(NOT FLAG_INDEX EQUAL -1)
+      return()
+    endif()
+    string(REGEX REPLACE "[^a-zA-Z0-9]" "_" varname "${flag}")
+    check_cxx_compiler_flag("${flag}" CMAKE_CXX_COMPILER_SUPPORTS_${varname})
+    if(CMAKE_CXX_COMPILER_SUPPORTS_${varname})
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
+      return()
+    endif()
+  endforeach()
+endfunction()
+
+#------------------------------------------------------------------------------
+# Add specified flags, if supported by the compiler
+function(qte_add_cxx_flags)
+  foreach(flag ${ARGN})
+    qte_add_cxx_flags_priority("${flag}")
+  endforeach()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" PARENT_SCOPE)
+endfunction()
+
+#------------------------------------------------------------------------------
+# Test for required C++ feature
+function(qte_test_cxx_feature feature message)
+  qte_verbose_try_compile(${feature}_WORKS ${feature}.cxx "${message}")
+  if(NOT ${feature}_WORKS)
+    message(FATAL_ERROR
+      "A required compiler feature is missing. "
+      "Try adjusting your CMAKE_CXX_FLAGS or using a newer compiler."
+    )
+  endif()
+endfunction()
+
+###############################################################################
+
 option(BUILD_SHARED_LIBS "Build shared libraries" ON)
 option(QTE_ENABLE_PYTHON "Enable qtExtensions Python (PySide) bindings" OFF)
 
+# Set compiler flags for exports (e.g. -fvisibility=hidden)
 add_compiler_export_flags()
+
+# Set extra compiler flags
+if(NOT MSVC)
+  # Determine what flags (if any) are needed for required C++ language support
+  # Note: MSVC always uses latest known C++ extensions
+  qte_add_cxx_flags_priority(-std=c++11 -std=c++0x)
+
+  # Turn on extra warnings if requested
+  option(VISGUI_EXTRA_WARNINGS "Enable extra warnings" ON)
+  if(VISGUI_EXTRA_WARNINGS)
+    qte_add_cxx_flags(
+      -Wall -Wextra -Wunused -Wuninitialized -Wctor-dtor-privacy
+      -Wcast-align -Wlogical-op -Wpointer-arith
+    )
+  endif()
+  # Turn on additional warnings-as-errors
+  qte_add_cxx_flags(
+    # Missing 'return'
+    -Werror=return-type
+    # Improper use of 'virtual'
+    -Werror=non-virtual-dtor -Werror=overloaded-virtual
+    # Casting away 'const' without const_cast (i.e. via a C-style cast)
+    -Werror=cast-qual
+    # Use of ill-formed constructs
+    -Werror=narrowing
+    # Local and member variable initialization issues
+    -Werror=init-self -Werror=reorder
+  )
+endif()
+
+# Test for required compiler features
+qte_test_cxx_feature(
+  auto "compiler supports 'auto' as a type declaration")
+qte_test_cxx_feature(
+  defaulted-ctor "compiler supports explicitly defaulted constructors")
