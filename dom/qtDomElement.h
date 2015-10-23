d4d2da69590375a2003b10a4d964b40218bbae33
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-10-23 14:36:18 -0400

    Fix error in `qtDomElement` example

--- dom/qtDomElement.h
+++ dom/qtDomElement.h
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2012 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2015 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -67,10 +67,9 @@
 ///            .add(qtDomElement(doc, "title").addText("Example")));
 ///
 /// // Create body
-/// qtDomElement body(doc, "body");
-/// body.add(qtDomElement(doc, "p", "content")
-///            .addText("Hello, world!"));
 /// html.add(qtDomElement(doc, "body")
+///            .add(qtDomElement(doc, "p", "content")
+///                   .addText("Hello, world!")));
 ///
 /// // Set document content
 /// doc.appendChild(html);
