7bf2d7f231ba64d207c450c3fbeabc79b0160ba5
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-12-16 14:15:28 -0500

    Update style for `qtGradient`
    Adjust code style of `qtGradient.{cpp,h}` to match new conventions.

--- util/qtGradient.cpp
+++ util/qtGradient.cpp
@@ -21,83 +21,83 @@ QTE_IMPLEMENT_D_FUNC_SHARED(qtGradient)
 class qtGradientData : public QSharedData
 {
 public:
-  QMap<qreal, qtGradient::Stop> stops;
-  qtGradient::InterpolationMode interpolateMode;
-  qtGradient::Spread spread;
+    QMap<qreal, qtGradient::Stop> stops;
+    qtGradient::InterpolationMode interpolateMode;
+    qtGradient::Spread spread;
 
-  QColor blend(const QColor& a, const QColor& b, qreal t) const;
-  QColor linearBlend(const QColor& a, const QColor& b,
-                     qreal t, qreal w) const;
-  QColor cubicBlend(const qtGradient::Stop& a, const qtGradient::Stop& b,
-                    const qtGradient::Stop& c, const qtGradient::Stop& d,
-                    qreal t) const;
+    QColor blend(QColor const& a, QColor const& b, qreal t) const;
+    QColor linearBlend(QColor const& a, QColor const& b,
+                       qreal t, qreal w) const;
+    QColor cubicBlend(qtGradient::Stop const& a, qtGradient::Stop const& b,
+                      qtGradient::Stop const& c, qtGradient::Stop const& d,
+                      qreal t) const;
 
-  QColor::Spec blendSpace() const;
+    QColor::Spec blendSpace() const;
 };
 
 //-----------------------------------------------------------------------------
 QColor::Spec qtGradientData::blendSpace() const
 {
-  switch (this->interpolateMode & qtGradient::InterpolateColorspaceMask)
+    switch (interpolateMode & qtGradient::InterpolateColorspaceMask)
     {
-    case qtGradient::InterpolateCmyk:
-      return QColor::Cmyk;
-    case qtGradient::InterpolateHsv:
-      return QColor::Hsv;
-    case qtGradient::InterpolateHsl:
-      return QColor::Hsl;
-    default: // RGB
-      return QColor::Rgb;
+        case qtGradient::InterpolateCmyk:
+            return QColor::Cmyk;
+        case qtGradient::InterpolateHsv:
+            return QColor::Hsv;
+        case qtGradient::InterpolateHsl:
+            return QColor::Hsl;
+        default: // RGB
+            return QColor::Rgb;
     }
 }
 
 //-----------------------------------------------------------------------------
-QColor qtGradientData::blend(const QColor& a, const QColor& b, qreal t) const
+QColor qtGradientData::blend(QColor const& a, QColor const& b, qreal t) const
 {
-  return qtColorUtil::blend(a, b, t, this->blendSpace());
+    return qtColorUtil::blend(a, b, t, blendSpace());
 }
 
 //-----------------------------------------------------------------------------
 QColor qtGradientData::linearBlend(
-  const QColor& a, const QColor& b, qreal t, qreal w) const
+    QColor const& a, QColor const& b, qreal t, qreal w) const
 {
-  // Apply weighting
-  t = (t > w ? qtColorUtil::blend(0.5, 1.0, (t - w) / (1.0 - w))
-             : qtColorUtil::blend(0.0, 0.5, t / w));
+    // Apply weighting
+    t = (t > w ? qtColorUtil::blend(0.5, 1.0, (t - w) / (1.0 - w))
+         : qtColorUtil::blend(0.0, 0.5, t / w));
 
-  // Perform blend
-  return this->blend(a, b, t);
+    // Perform blend
+    return blend(a, b, t);
 }
 
 //-----------------------------------------------------------------------------
 QColor qtGradientData::cubicBlend(
-  const qtGradient::Stop& a, const qtGradient::Stop& b,
-  const qtGradient::Stop& c, const qtGradient::Stop& d, qreal t) const
+    qtGradient::Stop const& a, qtGradient::Stop const& b,
+    qtGradient::Stop const& c, qtGradient::Stop const& d, qreal t) const
 {
-  // Calculate intermediary stops
-  const QColor cb = b.Color, cc = c.Color;
-  const QColor ca = this->blend(a.Color, cb, 0.5);
-  const QColor cd = this->blend(d.Color, cc, 0.5);
-  const QColor cm = this->blend(cb, cc, 0.5);
-  const qreal w = b.Weight;
-  const qreal pb = b.Position, pc = c.Position;
-  const qreal pa = qtColorUtil::blend(a.Position, pb, a.Weight);
-  const qreal pd = qtColorUtil::blend(d.Position, pc, c.Weight);
-  const qreal pm = qtColorUtil::blend(pb, pc, w);
+    // Calculate intermediary stops
+    auto const& cb = b.Color, cc = c.Color;
+    auto const& ca = blend(a.Color, cb, 0.5);
+    auto const& cd = blend(d.Color, cc, 0.5);
+    auto const& cm = blend(cb, cc, 0.5);
+    auto const w = b.Weight;
+    auto const pb = b.Position, pc = c.Position;
+    auto const pa = qtColorUtil::blend(a.Position, pb, a.Weight);
+    auto const pd = qtColorUtil::blend(d.Position, pc, c.Weight);
+    auto const pm = qtColorUtil::blend(pb, pc, w);
 
-  if (t > w)
+    if (t > w)
     {
-    t = (t - w) / (1.0 - w);
-    t = qtColorUtil::blend(pb, pm, pc, pd, t);
-    t = qBound(0.0, (t - pm) / (pc - pm), 1.0);
-    return qtColorUtil::blend(cb, cm, cc, cd, t, this->blendSpace());
+        t = (t - w) / (1.0 - w);
+        t = qtColorUtil::blend(pb, pm, pc, pd, t);
+        t = qBound(0.0, (t - pm) / (pc - pm), 1.0);
+        return qtColorUtil::blend(cb, cm, cc, cd, t, blendSpace());
     }
-  else
+    else
     {
-    t /= w;
-    t = qtColorUtil::blend(pa, pb, pm, pc, t);
-    t = qBound(0.0, (t - pb) / (pm - pb), 1.0);
-    return qtColorUtil::blend(ca, cb, cm, cc, t, this->blendSpace());
+        t /= w;
+        t = qtColorUtil::blend(pa, pb, pm, pc, t);
+        t = qBound(0.0, (t - pb) / (pm - pb), 1.0);
+        return qtColorUtil::blend(ca, cb, cm, cc, t, blendSpace());
     }
 }
 
@@ -110,27 +110,27 @@ QColor qtGradientData::cubicBlend(
 //-----------------------------------------------------------------------------
 qtGradient::qtGradient() : d_ptr(new qtGradientData)
 {
-  QTE_D_MUTABLE(qtGradient);
-  d->interpolateMode = qtGradient::InterpolateLinear |
-                       qtGradient::InterpolateRgb;
-  d->spread = QGradient::PadSpread;
-  this->insertStop(0.0, Qt::black);
-  this->insertStop(1.0, Qt::white);
+    QTE_D_MUTABLE();
+    d->interpolateMode = qtGradient::InterpolateLinear |
+                         qtGradient::InterpolateRgb;
+    d->spread = QGradient::PadSpread;
+    insertStop(0.0, Qt::black);
+    insertStop(1.0, Qt::white);
 }
 
 //-----------------------------------------------------------------------------
-qtGradient::qtGradient(const QList<Stop>& stops, InterpolationMode im,
+qtGradient::qtGradient(QList<Stop> const& stops, InterpolationMode im,
                        QGradient::Spread spread, NormalizeMode nm)
-  : d_ptr(new qtGradientData)
+    : d_ptr{new qtGradientData}
 {
-  QTE_D_MUTABLE(qtGradient);
-  d->interpolateMode = im;
-  d->spread = spread;
-  this->setStops(stops, nm);
+    QTE_D_MUTABLE();
+    d->interpolateMode = im;
+    d->spread = spread;
+    setStops(stops, nm);
 }
 
 //-----------------------------------------------------------------------------
-qtGradient::qtGradient(const qtGradient& other) : d_ptr(other.d_ptr)
+qtGradient::qtGradient(qtGradient const& other) : d_ptr{other.d_ptr}
 {
 }
 
@@ -140,221 +140,202 @@ qtGradient::~qtGradient()
 }
 
 //-----------------------------------------------------------------------------
-qtGradient& qtGradient::operator=(const qtGradient& other)
+qtGradient& qtGradient::operator=(qtGradient const& other)
 {
-  this->d_ptr = other.d_ptr;
-  return *this;
+    d_ptr = other.d_ptr;
+    return *this;
 }
 
 //-----------------------------------------------------------------------------
 qtGradient::InterpolationMode qtGradient::interpolationMode() const
 {
-  QTE_D_SHARED(qtGradient);
-  return d->interpolateMode;
+    QTE_D_SHARED();
+    return d->interpolateMode;
 }
 
 //-----------------------------------------------------------------------------
 void qtGradient::setInterpolationMode(qtGradient::InterpolationMode im)
 {
-  QTE_D_MUTABLE(qtGradient);
-  d->interpolateMode = im;
+    QTE_D_MUTABLE();
+    d->interpolateMode = im;
 }
 
 //-----------------------------------------------------------------------------
 qtGradient::Spread qtGradient::spread() const
 {
-  QTE_D_SHARED(qtGradient);
-  return d->spread;
+    QTE_D_SHARED();
+    return d->spread;
 }
 
 //-----------------------------------------------------------------------------
 void qtGradient::setSpread(qtGradient::Spread spread)
 {
-  QTE_D_MUTABLE(qtGradient);
-  d->spread = spread;
+    QTE_D_MUTABLE();
+    d->spread = spread;
 }
 
 //-----------------------------------------------------------------------------
 QMap<qreal, qtGradient::Stop> qtGradient::stops() const
 {
-  QTE_D_SHARED(qtGradient);
-  return d->stops;
+    QTE_D_SHARED();
+    return d->stops;
 }
 
 //-----------------------------------------------------------------------------
-void qtGradient::setStops(const QList<qtGradient::Stop>& stops,
+void qtGradient::setStops(QList<qtGradient::Stop> const& stops,
                           qtGradient::NormalizeMode nm)
 {
-  QTE_D_MUTABLE(qtGradient);
+    QTE_D_MUTABLE();
 
-  // Handle empty set
-  if (stops.isEmpty())
+    // Handle empty set
+    if (stops.isEmpty())
     {
-    d->stops.clear();
-    return;
+        d->stops.clear();
+        return;
     }
 
-  // Handle set with exactly one stop
-  if (stops.count() == 1)
+    // Handle set with exactly one stop
+    if (stops.count() == 1)
     {
-    d->stops.clear();
-    d->stops.insert(0.0, stops.first());
-    return;
+        d->stops.clear();
+        d->stops.insert(0.0, stops.first());
+        return;
     }
 
-  // Convert to map
-  QMap<qreal, qtGradient::Stop> stopsMap;
-  foreach (auto const& stop, stops)
-    stopsMap.insert(stop.Position, stop);
+    // Convert to map
+    QMap<qreal, qtGradient::Stop> stopsMap;
+    foreach (auto const& stop, stops)
+        stopsMap.insert(stop.Position, stop);
 
-  // Handle 'regular' stop sets, based on normalization mode
-  if (nm == qtGradient::NormalizeStops)
+    // Handle 'regular' stop sets, based on normalization mode
+    if (nm == qtGradient::NormalizeStops)
     {
-    // Calculate coefficients to normalize stops to [0.0, 1.0]
-    double offset = stopsMap.constBegin().key();
-    double scale = 1.0 / ((--stopsMap.constEnd()).key() - offset);
+        // Calculate coefficients to normalize stops to [0.0, 1.0]
+        auto const offset = stopsMap.constBegin().key();
+        auto const scale = 1.0 / ((--stopsMap.constEnd()).key() - offset);
 
-    // Recreate stop set, normalized
-    d->stops.clear();
-    foreach (auto stop, stops)
-      {
-      stop.Position = (stop.Position - offset) * scale;
-      d->stops.insert(stop.Position, stop);
-      }
-    }
-  else
-    {
-    QMap<qreal, qtGradient::Stop>::iterator iter;
-
-    // Truncate stops outside of normalized range
-    while (!stopsMap.isEmpty() && (iter = stopsMap.begin()).key() < 0.0)
-      {
-      stopsMap.erase(iter);
-      }
-    while (!stopsMap.isEmpty() && (iter = stopsMap.end()).key() > 1.0)
-      {
-      stopsMap.erase(iter);
-      }
-
-    // Add stops at 0.0, 1.0 if needed
-    if (!stopsMap.isEmpty())
-      {
-      if ((iter = stopsMap.begin()).key() > 0.0)
+        // Recreate stop set, normalized
+        d->stops.clear();
+        foreach (auto stop, stops)
         {
-        stopsMap.insert(0.0, iter.value());
+            stop.Position = (stop.Position - offset) * scale;
+            d->stops.insert(stop.Position, stop);
         }
-      if ((iter = stopsMap.end()).key() < 1.0)
-        {
-        stopsMap.insert(1.0, iter.value());
-        }
-      }
+    }
+    else
+    {
+        QMap<qreal, qtGradient::Stop>::iterator iter;
 
-    // Set stops to modified set
-    d->stops = stopsMap;
+        // Truncate stops outside of normalized range
+        while (!stopsMap.isEmpty() && (iter = stopsMap.begin()).key() < 0.0)
+            stopsMap.erase(iter);
+        while (!stopsMap.isEmpty() && (iter = stopsMap.end()).key() > 1.0)
+            stopsMap.erase(iter);
+
+        // Add stops at 0.0, 1.0 if needed
+        if (!stopsMap.isEmpty())
+        {
+            if ((iter = stopsMap.begin()).key() > 0.0)
+                stopsMap.insert(0.0, iter.value());
+            if ((iter = stopsMap.end()).key() < 1.0)
+                stopsMap.insert(1.0, iter.value());
+        }
+
+        // Set stops to modified set
+        d->stops = stopsMap;
     }
 }
 
 //-----------------------------------------------------------------------------
 bool qtGradient::insertStop(qtGradient::Stop stop)
 {
-  if (!(0.0 < stop.Position || stop.Position < 1.0))
-    {
-    return false;
-    }
+    if (!(0.0 < stop.Position || stop.Position < 1.0))
+        return false;
 
-  QTE_D_MUTABLE(qtGradient);
-  d->stops.insert(stop.Position, stop);
-  return true;
+    QTE_D_MUTABLE();
+    d->stops.insert(stop.Position, stop);
+    return true;
 }
 
 //-----------------------------------------------------------------------------
 bool qtGradient::removeStop(qreal position)
 {
-  QTE_D_MUTABLE(qtGradient);
-  return (d->stops.remove(position) > 0);
+    QTE_D_MUTABLE();
+    return (d->stops.remove(position) > 0);
 }
 
 //-----------------------------------------------------------------------------
 QColor qtGradient::at(qreal pos) const
 {
-  QTE_D_SHARED(qtGradient);
+    QTE_D_SHARED();
 
-  if (d->stops.isEmpty())
-    {
-    return Qt::transparent;
-    }
-  else if (d->stops.count() < 2)
-    {
-    return d->stops.begin().value().Color;
-    }
+    if (d->stops.isEmpty())
+        return Qt::transparent;
+    if (d->stops.count() < 2)
+        return d->stops.begin().value().Color;
 
-  // Apply spread to get normalized position
-  switch (d->spread)
+    // Apply spread to get normalized position
+    switch (d->spread)
     {
-    case QGradient::RepeatSpread:
-      pos = fmod(pos, 1.0);
-      (pos < 0.0) && (pos = 1.0 + pos);
-      break;
-    case QGradient::ReflectSpread:
-      pos = fabs(fmod(pos, 2.0));
-      (pos > 1.0) && (pos = 2.0 - pos);
-      break;
-    default:
-      pos = qBound(0.0, pos, 1.0);
-      break;
+        case QGradient::RepeatSpread:
+            pos = fmod(pos, 1.0);
+            (pos < 0.0) && (pos = 1.0 + pos);
+            break;
+        case QGradient::ReflectSpread:
+            pos = fabs(fmod(pos, 2.0));
+            (pos > 1.0) && (pos = 2.0 - pos);
+            break;
+        default:
+            pos = qBound(0.0, pos, 1.0);
+            break;
     }
 
-  // Find next stop
-  typedef QMap<qreal, qtGradient::Stop>::const_iterator Iterator;
-  Iterator su = d->stops.lowerBound(pos);
+    // Find next stop
+    auto const su = d->stops.lowerBound(pos);
 
-  // Check for exact (or 'close enough') match
-  if (qFuzzyCompare(pos, su.key()))
+    // Check for exact (or 'close enough') match
+    if (qFuzzyCompare(pos, su.key()))
+        return su.value().Color;
+
+    // Find previous stop and calculate relative position
+    auto const sl = su - 1;
+    auto const rpos = (pos - sl.key()) / (su.key() - sl.key());
+
+    // Check (again) for exact (or 'close enough') match, this time against the
+    // previous stop (in case we are off 'just enough' that lowerBound didn't
+    // consider it a match)
+    if (qFuzzyCompare(pos, sl.key()))
+        return sl.value().Color;
+
+    // Calculate blended color
+    switch (d->interpolateMode & qtGradient::InterpolateFunctionMask)
     {
-    return su.value().Color;
-    }
+        case qtGradient::InterpolateDiscrete:
+            return (rpos < sl->Weight ? sl->Color : su->Color);
 
-  // Find previous stop and calculate relative position
-  Iterator sl = su - 1;
-  const qreal rpos = (pos - sl.key()) / (su.key() - sl.key());
+        case qtGradient::InterpolateCubic:
+        {
+            auto const sll = (sl == d->stops.begin() ? sl : sl - 1);
+            auto const suu = (su + 1 == d->stops.end() ? su : su + 1);
+            return d->cubicBlend(*sll, *sl, *su, *suu, rpos);
+        }
 
-  // Check (again) for exact (or 'close enough') match, this time against the
-  // previous stop (in case we are off 'just enough' that lowerBound didn't
-  // consider it a match)
-  if (qFuzzyCompare(pos, sl.key()))
-    {
-    return sl.value().Color;
-    }
-
-  // Calculate blended color
-  switch (d->interpolateMode & qtGradient::InterpolateFunctionMask)
-    {
-    case qtGradient::InterpolateDiscrete:
-      return (rpos < sl->Weight ? sl->Color : su->Color);
-    case qtGradient::InterpolateCubic:
-      {
-      Iterator sll = (sl == d->stops.begin() ? sl : sl - 1);
-      Iterator suu = (su + 1 == d->stops.end() ? su : su + 1);
-      return d->cubicBlend(*sll, *sl, *su, *suu, rpos);
-      }
-    default: // qtGradient::InterpolateLinear
-      return d->linearBlend(sl->Color, su->Color, rpos, sl->Weight);
+        default: // qtGradient::InterpolateLinear
+            return d->linearBlend(sl->Color, su->Color, rpos, sl->Weight);
     }
 }
 
 //-----------------------------------------------------------------------------
 QList<QColor> qtGradient::render(int size) const
 {
-  QList<QColor> out;
-  out.reserve(size);
+    QList<QColor> out;
+    out.reserve(size);
 
-  qreal k = 1.0 / (size - 1);
-  foreach (auto const i, qtIndexRange(size))
-    {
-    out.append(this->at(i * k));
-    }
+    auto const k = static_cast<qreal>(1.0 / (size - 1));
+    foreach (auto const i, qtIndexRange(size))
+        out.append(at(static_cast<qreal>(i) * k));
 
-  return out;
+    return out;
 }
 
 //END qtGradient
--- util/qtGradient.h
+++ util/qtGradient.h
@@ -1,5 +1,5 @@
 /*ckwg +5
- * Copyright 2013 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2015 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
@@ -18,72 +18,72 @@ class qtGradientData;
 class QTE_EXPORT qtGradient
 {
 public:
-  struct Stop
+    struct Stop
     {
-    Stop(qreal position = 0.0,
-         QColor color = Qt::transparent,
-         qreal weight = 0.5)
-      : Color(color), Position(position), Weight(weight)
-      {}
+        Stop(qreal position = 0.0,
+             QColor color = Qt::transparent,
+             qreal weight = 0.5)
+            : Color(color), Position(position), Weight(weight)
+        {}
 
-    QColor Color;
-    qreal Position;
-    qreal Weight;
+        QColor Color;
+        qreal Position;
+        qreal Weight;
     };
 
-  enum InterpolationFlag
+    enum InterpolationFlag
     {
-    InterpolateDiscrete = 0x0,
-    InterpolateLinear = 0x1,
-    InterpolateCubic = 0x2,
-    InterpolateRgb = 0x00,
-    InterpolateCmyk = 0x10,
-    InterpolateHsv = 0x20,
-    InterpolateHsl = 0x30,
-    InterpolateFunctionMask = 0xf,
-    InterpolateColorspaceMask = 0xf0
+        InterpolateDiscrete = 0x0,
+        InterpolateLinear = 0x1,
+        InterpolateCubic = 0x2,
+        InterpolateRgb = 0x00,
+        InterpolateCmyk = 0x10,
+        InterpolateHsv = 0x20,
+        InterpolateHsl = 0x30,
+        InterpolateFunctionMask = 0xf,
+        InterpolateColorspaceMask = 0xf0
     };
-  Q_DECLARE_FLAGS(InterpolationMode, InterpolationFlag)
+    Q_DECLARE_FLAGS(InterpolationMode, InterpolationFlag)
 
-  typedef QGradient::Spread Spread;
+    typedef QGradient::Spread Spread;
 
-  enum NormalizeMode
+    enum NormalizeMode
     {
-    NormalizeStops,
-    PadStops
+        NormalizeStops,
+        PadStops
     };
 
-  qtGradient();
-  qtGradient(const QList<Stop>& stops,
-             InterpolationMode = InterpolateLinear,
-             Spread = QGradient::PadSpread,
-             NormalizeMode = NormalizeStops);
-  qtGradient(const qtGradient&);
-  ~qtGradient();
+    qtGradient();
+    qtGradient(QList<Stop> const& stops,
+               InterpolationMode = InterpolateLinear,
+               Spread = QGradient::PadSpread,
+               NormalizeMode = NormalizeStops);
+    qtGradient(const qtGradient&);
+    ~qtGradient();
 
-  qtGradient& operator=(const qtGradient&);
+    qtGradient& operator=(const qtGradient&);
 
-  InterpolationMode interpolationMode() const;
-  void setInterpolationMode(InterpolationMode);
-  Spread spread() const;
-  void setSpread(Spread);
+    InterpolationMode interpolationMode() const;
+    void setInterpolationMode(InterpolationMode);
+    Spread spread() const;
+    void setSpread(Spread);
 
-  QMap<qreal, Stop> stops() const;
-  void setStops(const QList<Stop>&, NormalizeMode = NormalizeStops);
-  bool insertStop(Stop stop);
-  bool removeStop(qreal position);
+    QMap<qreal, Stop> stops() const;
+    void setStops(QList<Stop> const&, NormalizeMode = NormalizeStops);
+    bool insertStop(Stop stop);
+    bool removeStop(qreal position);
 
-  bool insertStop(qreal position, QColor color, qreal weight = 0.5)
-    { return this->insertStop(qtGradient::Stop(position, color, weight)); }
+    bool insertStop(qreal position, QColor color, qreal weight = 0.5)
+    { return insertStop(qtGradient::Stop(position, color, weight)); }
 
-  QColor at(qreal) const;
-  QList<QColor> render(int size) const;
+    QColor at(qreal) const;
+    QList<QColor> render(int size) const;
 
 protected:
-  QTE_DECLARE_SHARED_PTR(qtGradient)
+    QTE_DECLARE_SHARED_PTR(qtGradient)
 
 private:
-  QTE_DECLARE_SHARED(qtGradient)
+    QTE_DECLARE_SHARED(qtGradient)
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(qtGradient::InterpolationMode)
