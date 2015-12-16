aba98fa32ee1e3d579bcb9db71b6dad9b123270d
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-12-16 15:51:05 -0500

    Enable CMake automoc
    Update build logic to leverage CMake's automoc feature, rather than
    listing all the headers by hand. (This requires creating some dummy
    source files. For the same reason, we are still partially using explicit
    wrapping for the Designer plugin, as the interface classes for this tend
    to be header-only.)

---
+++ util/qtDockControllerPrivate.cpp
@@ -0,0 +1,4 @@
+// This file exists only to trigger CMake automoc to include the MOC source for
+// the qtDockControllerPrivate class, which requires that the source file name
+// exactly match the header file name. As a private class, the implementation
+// can be found in the source file for the public class.
