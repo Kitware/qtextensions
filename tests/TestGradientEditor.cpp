c0d4be22f29517c38f0fe5a8738561092aa79b04
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-12-24 14:14:28 -0500

    Output gradient code on accept
    Modify the gradient editor test program to output code to recreate the
    gradient when the dialog is accepted.

--- tests/TestGradientEditor.cpp
+++ tests/TestGradientEditor.cpp
@@ -5,8 +5,11 @@
  */
 
 #include <QApplication>
+#include <QDebug>
 
 #include "../dialogs/qtGradientEditor.h"
+
+#include "../util/qtGradient.h"
 
 //-----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -15,5 +18,21 @@ int main(int argc, char** argv)
 
     qtGradientEditor editor;
 
-    return editor.exec();
+    auto const result = editor.exec();
+    if (result == QDialog::Accepted)
+    {
+        foreach (auto const stop, editor.gradient().stops())
+        {
+            qDebug().nospace()
+                << "gradient.insertStop("
+                << stop.position << ", QColor("
+                << stop.color.red() << ", "
+                << stop.color.green() << ", "
+                << stop.color.blue() << "));";
+        }
+        return 0;
+    }
+
+    // Dialog canceled
+    return 1;
 }
