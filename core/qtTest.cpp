73cfdb642c541620922d179397642fe37779afba
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-05-24 14:26:29 -0400

    Update style for `qtTest`

--- core/qtTest.cpp
+++ core/qtTest.cpp
@@ -25,26 +25,20 @@ namespace // anonymous
 static void qtMessageHandler(
     QtMsgType, QString const& msg, qtTest::StreamPointer stream)
 {
-  static QThreadStorage<qtTest::StreamPointer*> ts;
-  qtTest::StreamPointer* tsp = ts.localData();
+    static QThreadStorage<qtTest::StreamPointer*> ts;
+    qtTest::StreamPointer* tsp = ts.localData();
 
-  if (stream || msg == 0)
+    if (stream || msg == 0)
     {
-    if (tsp)
-      {
-      (*tsp) = stream;
-      }
-    else
-      {
-      ts.setLocalData(new qtTest::StreamPointer(stream));
-      }
+        if (tsp)
+            (*tsp) = stream;
+        else
+            ts.setLocalData(new qtTest::StreamPointer(stream));
     }
-  else
+    else
     {
-    if (tsp && *tsp)
-      {
-      (**tsp) << msg << '\n';
-      }
+        if (tsp && *tsp)
+            (**tsp) << msg << '\n';
     }
 }
 
@@ -54,16 +48,17 @@ static void qtMessageHandler(
 static void qtMessageHandler(
     QtMsgType type, QMessageLogContext const& context, QString const& msg)
 {
-  Q_UNUSED(context);
-  qtMessageHandler(type, msg, qtTest::StreamPointer());
+    Q_UNUSED(context);
+    qtMessageHandler(type, msg, qtTest::StreamPointer());
 }
 
 #else
 
 //-----------------------------------------------------------------------------
-static void qtMessageHandler(QtMsgType type, const char* msg)
+static void qtMessageHandler(QtMsgType type, char const* msg)
 {
-  qtMessageHandler(type, QString::fromLocal8Bit(msg), qtTest::StreamPointer());
+    qtMessageHandler(type, QString::fromLocal8Bit(msg),
+                     qtTest::StreamPointer());
 }
 
 #endif
@@ -74,58 +69,54 @@ static void qtMessageHandler(QtMsgType type, const char* msg)
 class qtTestPrivate
 {
 public:
-  qtTestPrivate()
-    : testResult(0), collectedResult(0), totalTests(0), totalPassed(0),
-      err(new QTextStream(stderr, QIODevice::WriteOnly))
+    qtTestPrivate() : err{new QTextStream(stderr, QIODevice::WriteOnly)}
     {}
 
-  int testResult, collectedResult;
-  int totalTests, totalPassed;
-  qtTest::StreamPointer err;
-  QTextStream* out;
-  QStack<qtTest::StreamPointer> streamStack;
-  QStack<QString> traceStack;
+    int testResult = 0, collectedResult = 0;
+    int totalTests = 0, totalPassed = 0;
+    qtTest::StreamPointer err;
+    QTextStream* out;
+    QStack<qtTest::StreamPointer> streamStack;
+    QStack<QString> traceStack;
 };
 
 QTE_IMPLEMENT_D_FUNC(qtTest)
 
 //-----------------------------------------------------------------------------
-qtTest::qtTest() : d_ptr(new qtTestPrivate)
+qtTest::qtTest() : d_ptr{new qtTestPrivate}
 {
-  QTE_D(qtTest);
+    QTE_D();
 
-  qtTest::init();
+    qtTest::init();
 
-  this->pushMessageStream(d->err);
+    this->pushMessageStream(d->err);
 }
 
 //-----------------------------------------------------------------------------
 qtTest::~qtTest()
 {
-  QTE_D(qtTest);
+    QTE_D();
 
-  (*d->err) << '\n' << d->totalPassed << " of " << d->totalTests << " passed";
-  if (d->totalPassed < d->totalTests)
-    {
-    (*d->err) << ", " << (d->totalTests - d->totalPassed) << " failed";
-    }
-  (*d->err) << '\n';
-  d->err->flush();
+    (*d->err) << '\n' << d->totalPassed << " of " << d->totalTests << " passed";
+    if (d->totalPassed < d->totalTests)
+        (*d->err) << ", " << (d->totalTests - d->totalPassed) << " failed";
+    (*d->err) << '\n';
+    d->err->flush();
 
-  delete d;
+    delete d;
 }
 
 //-----------------------------------------------------------------------------
 void qtTest::init()
 {
-  static volatile bool initialized = false;
-  if (!initialized)
+    static volatile bool initialized = false;
+    if (!initialized)
     {
-    initialized = true;
+        initialized = true;
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
-    qInstallMessageHandler(&qtMessageHandler);
+        qInstallMessageHandler(&qtMessageHandler);
 #else
-    qInstallMsgHandler(&qtMessageHandler);
+        qInstallMsgHandler(&qtMessageHandler);
 #endif
     }
 }
@@ -133,239 +124,223 @@ void qtTest::init()
 //-----------------------------------------------------------------------------
 int qtTest::result() const
 {
-  QTE_D_CONST(qtTest);
-  return d->collectedResult;
+    QTE_D();
+    return d->collectedResult;
 }
 
 //-----------------------------------------------------------------------------
-int qtTest::runSuite(const QString& name, qtTest::Suite suite)
+int qtTest::runSuite(QString const& name, qtTest::Suite suite)
 {
-  QTE_D(qtTest);
+    QTE_D();
 
-  // Display banner
-  ++d->totalTests;
-  QString buffer(60 - name.length(), '.');
-  (*d->err) << name << ' ' << buffer;
+    // Display banner
+    ++d->totalTests;
+    QString buffer{60 - name.length(), '.'};
+    (*d->err) << name << ' ' << buffer;
 
-  // Register stream for QDebug handler
-  buffer.clear();
-  StreamPointer out(new QTextStream(&buffer));
-  d->out = out.data();
-  uint token = this->pushMessageStream(out);
+    // Register stream for QDebug handler
+    buffer.clear();
+    StreamPointer out{new QTextStream{&buffer}};
+    d->out = out.data();
+    auto const token = this->pushMessageStream(out);
 
-  // Execute the test
-  d->testResult = 0;
-  int result = (*suite)(*this) | d->testResult;
+    // Execute the test
+    d->testResult = 0;
+    int result = (*suite)(*this) | d->testResult;
 
-  // Restore stream for QDebug handler
-  this->popMessageStream(token);
-  d->out = d->err.data();
+    // Restore stream for QDebug handler
+    this->popMessageStream(token);
+    d->out = d->err.data();
 
-  // Print test result and output
-  (*d->err) << (result ? "***Failed" : "   Passed") << '\n' << buffer;
+    // Print test result and output
+    (*d->err) << (result ? "***Failed" : "   Passed") << '\n' << buffer;
 
-  // Update statistics
-  if (result == 0)
-    {
-    ++d->totalPassed;
-    }
-  d->collectedResult |= result;
+    // Update statistics
+    if (result == 0)
+        ++d->totalPassed;
+    d->collectedResult |= result;
 
-  return result;
+    return result;
 }
 
 //-----------------------------------------------------------------------------
 QTextStream& qtTest::out()
 {
-  QTE_D(qtTest);
-  return (d->out ? *d->out : *d->err);
+    QTE_D();
+    return (d->out ? *d->out : *d->err);
 }
 
 //-----------------------------------------------------------------------------
 uint qtTest::pushMessageStream(qtTest::StreamPointer stream)
 {
-  QTE_D(qtTest);
+    QTE_D();
 
-  // Register new stream with handler
-  qtMessageHandler(QtSystemMsg, 0, stream);
+    // Register new stream with handler
+    qtMessageHandler(QtSystemMsg, 0, stream);
 
-  // Add stream to stack
-  uint token = d->streamStack.count();
-  d->streamStack.push(stream);
-  return token;
+    // Add stream to stack
+    uint token = static_cast<uint>(d->streamStack.count());
+    d->streamStack.push(stream);
+    return token;
 }
 
 //-----------------------------------------------------------------------------
 void qtTest::popMessageStream(uint token)
 {
-  QTE_D(qtTest);
+    QTE_D();
 
-  // Remove stream(s) from stack to get back to token
-  while (uint(d->streamStack.count()) > token)
-    {
-    d->streamStack.pop();
-    }
+    // Remove stream(s) from stack to get back to token
+    while (static_cast<uint>(d->streamStack.count()) > token)
+        d->streamStack.pop();
 
-  // Register old stream with handler
-  StreamPointer stream;
-  if (d->streamStack.count())
-    {
-    stream = d->streamStack.top();
-    }
-  qtMessageHandler(QtSystemMsg, 0, stream);
+    // Register old stream with handler
+    StreamPointer stream;
+    if (d->streamStack.count())
+        stream = d->streamStack.top();
+    qtMessageHandler(QtSystemMsg, 0, stream);
 }
 
 //-----------------------------------------------------------------------------
 QString qtTest::where() const
 {
-  QTE_D_CONST(qtTest);
-  if (!d->traceStack.isEmpty())
+    QTE_D();
+    if (!d->traceStack.isEmpty())
     {
-    QString trace = "in " + d->traceStack.top() + '\n';
-    for (int i = d->traceStack.count() - 2; i >= 0; --i)
-      {
-      trace += "  from " + d->traceStack[i] + '\n';
-      }
-    return trace;
+        QString trace = "in " + d->traceStack.top() + '\n';
+        for (int i = d->traceStack.count() - 2; i >= 0; --i)
+            trace += "  from " + d->traceStack[i] + '\n';
+        return trace;
     }
   return {};
 }
 
 //-----------------------------------------------------------------------------
 int qtTest::testEqual(float expectedValue, float actualValue,
-                      const QString& exprText,
-                      int line, const char* file, const char* func)
+                      QString const& exprText,
+                      int line, char const* file, char const* func)
 {
-  if (!qFuzzyCompare(actualValue, expectedValue))
+    if (!qFuzzyCompare(actualValue, expectedValue))
     {
-    return this->failedEqualityTest(expectedValue, actualValue, exprText,
-                                    line, file, func);
+        return this->failedEqualityTest(expectedValue, actualValue, exprText,
+                                        line, file, func);
     }
-  return 0;
+    return 0;
 }
 
 //-----------------------------------------------------------------------------
 int qtTest::testEqual(double expectedValue, double actualValue,
-                      const QString& exprText,
-                      int line, const char* file, const char* func)
+                      QString const& exprText,
+                      int line, char const* file, char const* func)
 {
-  if (!qFuzzyCompare(actualValue, expectedValue))
+    if (!qFuzzyCompare(actualValue, expectedValue))
     {
-    return this->failedEqualityTest(expectedValue, actualValue, exprText,
-                                    line, file, func);
+        return this->failedEqualityTest(expectedValue, actualValue, exprText,
+                                        line, file, func);
     }
-  return 0;
+    return 0;
 }
 
 //-----------------------------------------------------------------------------
 int qtTest::failedEqualityTest(
-  const std::string& expectedValue, const std::string& actualValue,
-  const QString& exprText, int line, const char* file, const char* func)
+    std::string const& expectedValue, std::string const& actualValue,
+    QString const& exprText, int line, char const* file, char const* func)
 {
-  return this->failedEqualityTest(qtString(expectedValue),
-                                  qtString(actualValue),
-                                  exprText, line, file, func);
+    return this->failedEqualityTest(qtString(expectedValue),
+                                    qtString(actualValue),
+                                    exprText, line, file, func);
 }
 
 //-----------------------------------------------------------------------------
 int qtTest::testResult() const
 {
-  QTE_D_CONST(qtTest);
-  return d->testResult;
+    QTE_D();
+    return d->testResult;
 }
 
 //-----------------------------------------------------------------------------
 int qtTest::setTestResult(int result)
 {
-  QTE_D(qtTest);
-  return (d->testResult |= result);
+    QTE_D();
+    return (d->testResult |= result);
 }
 
 //-----------------------------------------------------------------------------
 class qtTestTracePrivate
 {
 public:
-  qtTest* test;
-  int depth;
+    qtTest* test;
+    int depth;
 };
 
 QTE_IMPLEMENT_D_FUNC(qtTestTrace)
 
 //-----------------------------------------------------------------------------
-qtTestTrace::qtTestTrace(qtTest& test, int line, const char* file,
-                         const char* func)
-  : d_ptr(new qtTestTracePrivate)
+qtTestTrace::qtTestTrace(qtTest& test, int line, char const* file,
+                         char const* func)
+    : d_ptr(new qtTestTracePrivate)
 {
-  QTE_D(qtTestTrace);
+    QTE_D();
 
-  d->test = &test;
+    d->test = &test;
 
-  if (line)
+    if (line)
     {
-    // Get trace size so we can remove ourselves later
-    qtTestPrivate* td = test.d_func();
-    d->depth = td->traceStack.count();
+        // Get trace size so we can remove ourselves later
+        auto* const td = test.d_func();
+        d->depth = td->traceStack.count();
 
-    // Get function name without return type, if function name was given
-    QString shortFunc;
-    if (func)
-      {
-      shortFunc = QString::fromLocal8Bit(func);
-      if (shortFunc.contains(' '))
+        // Get function name without return type, if function name was given
+        QString shortFunc;
+        if (func)
         {
-        QRegExp re("[^ (]+([(][^)]*[)])?$");
-        int n = re.indexIn(shortFunc);
-        if (n > 0)
-          {
-          shortFunc.remove(0, n);
-          }
+            shortFunc = QString::fromLocal8Bit(func);
+            if (shortFunc.contains(' '))
+            {
+                QRegExp const re{"[^ (]+([(][^)]*[)])?$"};
+                auto const n = re.indexIn(shortFunc);
+                if (n > 0)
+                    shortFunc.remove(0, n);
+            }
         }
-      }
 
-    // Get short file name, if file name was given
-    if (file)
-      {
-      QFileInfo fi(QString::fromLocal8Bit(file));
-      QString shortFile = fi.fileName();
-      QString trace("%2:%1");
-      trace = trace.arg(line).arg(shortFile);
-      if (func)
+        // Get short file name, if file name was given
+        if (file)
         {
-        td->traceStack.push(shortFunc + " (" + trace + ')');
+            QFileInfo const fi{QString::fromLocal8Bit(file)};
+            auto const& shortFile = fi.fileName();
+            auto trace = QString{"%2:%1"};
+            trace = trace.arg(line).arg(shortFile);
+            if (func)
+                td->traceStack.push(shortFunc + " (" + trace + ')');
+            else
+                td->traceStack.push(trace);
         }
-      else
+        else
         {
-        td->traceStack.push(trace);
+            if (func)
+            {
+                auto const trace = QString{"%2 (%1)"};
+                td->traceStack.push(trace.arg(line).arg(shortFunc));
+            }
+            else
+            {
+                td->traceStack.push(QString::number(line));
+            }
         }
-      }
-    else
-      {
-      if (func)
-        {
-        QString trace("%2 (%1)");
-        td->traceStack.push(trace.arg(line).arg(shortFunc));
-        }
-      else
-        {
-        td->traceStack.push(QString::number(line));
-        }
-      }
     }
 }
 
 //-----------------------------------------------------------------------------
 qtTestTrace::~qtTestTrace()
 {
-  QTE_D(qtTestTrace);
+    QTE_D();
 
-  if (d->depth >= 0)
+    if (d->depth >= 0)
     {
-    qtTestPrivate* td = d->test->d_func();
-    while (td->traceStack.count() > d->depth)
-      {
-      td->traceStack.pop();
-      }
+        qtTestPrivate* td = d->test->d_func();
+        while (td->traceStack.count() > d->depth)
+            td->traceStack.pop();
     }
 
-  delete d;
+    delete d;
 }
--- core/qtTest.h
+++ core/qtTest.h
@@ -1,11 +1,13 @@
 /*ckwg +5
- * Copyright 2015 by Kitware, Inc. All Rights Reserved. Please refer to
+ * Copyright 2018 by Kitware, Inc. All Rights Reserved. Please refer to
  * KITWARE_LICENSE.TXT for licensing information, or contact General Counsel,
  * Kitware, Inc., 28 Corporate Drive, Clifton Park, NY 12065.
  */
 
 #ifndef __qtTest_h
 #define __qtTest_h
+
+#include "qtGlobal.h"
 
 #include <QDebug>
 #include <QSharedPointer>
@@ -14,8 +16,6 @@
 
 #include <cstdio>
 
-#include "qtGlobal.h"
-
 class qtTestPrivate;
 class qtTestTracePrivate;
 
@@ -23,70 +23,70 @@ class qtTestTracePrivate;
 class QTE_EXPORT qtTest
 {
 public:
-  typedef QSharedPointer<QTextStream> StreamPointer;
-  typedef int (* Suite)(qtTest&);
+    using StreamPointer = QSharedPointer<QTextStream>;
+    using Suite = int(*)(qtTest&);
 
-  qtTest();
-  virtual ~qtTest();
+    qtTest();
+    virtual ~qtTest();
 
-  static void init();
+    static void init();
 
-  int result() const;
-  int runSuite(const QString& name, Suite);
+    int result() const;
+    int runSuite(QString const& name, Suite);
 
-  QTextStream& out();
-  uint pushMessageStream(StreamPointer);
-  void popMessageStream(uint token);
+    QTextStream& out();
+    uint pushMessageStream(StreamPointer);
+    void popMessageStream(uint token);
 
-  QString where() const;
+    QString where() const;
 
-  inline int test(
-    bool exprValue, const QString& exprText, int line = 0,
-    const char* file = 0, const char* func = 0);
-  template <typename T>
-  inline int testEqual(
-    T expectedValue, T actualValue, const QString& exprText,
-    int line = 0, const char* file = 0, const char* func = 0);
-  int testEqual(
-    float expectedValue, float actualValue, const QString& exprText,
-    int line = 0, const char* file = 0, const char* func = 0);
-  int testEqual(
-    double expectedValue, double actualValue, const QString& exprText,
-    int line = 0, const char* file = 0, const char* func = 0);
+    inline int test(
+        bool exprValue, QString const& exprText, int line = 0,
+        char const* file = 0, char const* func = 0);
+    template <typename T>
+    inline int testEqual(
+        T expectedValue, T actualValue, QString const& exprText,
+        int line = 0, char const* file = 0, char const* func = 0);
+    int testEqual(
+        float expectedValue, float actualValue, QString const& exprText,
+        int line = 0, char const* file = 0, char const* func = 0);
+    int testEqual(
+        double expectedValue, double actualValue, QString const& exprText,
+        int line = 0, char const* file = 0, char const* func = 0);
 
-  int testResult() const;
-  int setTestResult(int);
+    int testResult() const;
+    int setTestResult(int);
 
 protected:
-  QTE_DECLARE_PRIVATE_PTR(qtTest)
-  friend class qtTestTrace;
+    QTE_DECLARE_PRIVATE_PTR(qtTest)
+    friend class qtTestTrace;
 
-  template <typename T>
-  inline int failedEqualityTest(
-    T expectedValue, T actualValue, const QString& exprText,
-    int line, const char* file, const char* func);
-  int failedEqualityTest(
-    const std::string& expectedValue, const std::string& actualValue,
-    const QString& exprText, int line, const char* file, const char* func);
+    template <typename T>
+    inline int failedEqualityTest(
+        T expectedValue, T actualValue, QString const& exprText,
+        int line, char const* file, char const* func);
+    int failedEqualityTest(
+        std::string const& expectedValue, std::string const& actualValue,
+        QString const& exprText, int line, char const* file, char const* func);
 
 private:
-  QTE_DECLARE_PRIVATE(qtTest)
-  QTE_DISABLE_COPY(qtTest)
+    QTE_DECLARE_PRIVATE(qtTest)
+    QTE_DISABLE_COPY(qtTest)
 };
 
 //-----------------------------------------------------------------------------
 class QTE_EXPORT qtTestTrace
 {
 public:
-  qtTestTrace(qtTest& test, int line = 0, const char* file = 0,
-              const char* func = 0);
-  ~qtTestTrace();
+    qtTestTrace(qtTest& test, int line = 0, char const* file = 0,
+                char const* func = 0);
+    ~qtTestTrace();
 
 protected:
-  QTE_DECLARE_PRIVATE_PTR(qtTestTrace)
+    QTE_DECLARE_PRIVATE_PTR(qtTestTrace)
 
 private:
-  QTE_DECLARE_PRIVATE(qtTestTrace)
+    QTE_DECLARE_PRIVATE(qtTestTrace)
 };
 
 //-----------------------------------------------------------------------------
@@ -106,73 +106,76 @@ private:
 #endif
 
 #define _QT_TEST_WHERE_ \
-  __LINE__, __FILE__, _QT_TEST_FUNCTION_
+    __LINE__, __FILE__, _QT_TEST_FUNCTION_
 
 #define TEST(_expr) \
-  TEST_OBJECT_NAME.test((_expr), #_expr, _QT_TEST_WHERE_)
+    TEST_OBJECT_NAME.test((_expr), #_expr, _QT_TEST_WHERE_)
 #define TEST_EQUAL(_expr, _value) \
-  TEST_OBJECT_NAME.testEqual((_value), (_expr), #_expr, _QT_TEST_WHERE_)
+    TEST_OBJECT_NAME.testEqual((_value), (_expr), #_expr, _QT_TEST_WHERE_)
 #define TEST_CALL(_func, ...) do { \
-  qtTestTrace _trace(TEST_OBJECT_NAME, _QT_TEST_WHERE_); \
-  _func(TEST_OBJECT_NAME, __VA_ARGS__); \
-  } while(0)
+    qtTestTrace _trace{TEST_OBJECT_NAME, _QT_TEST_WHERE_}; \
+    _func(TEST_OBJECT_NAME, __VA_ARGS__); \
+    } while(0)
 
 #define TEST_QUIET_XFAIL(_expr) do { \
-  qtTest::StreamPointer _stream(new qtStringStream); \
-  uint _token = TEST_OBJECT_NAME.pushMessageStream(_stream); \
-  bool _test = (_expr); \
-  TEST_OBJECT_NAME.test(!_test, "(XFAIL) " #_expr, _QT_TEST_WHERE_); \
-  if(_test) \
-    TEST_OBJECT_NAME.out() << *_stream->string(); \
-  TEST_OBJECT_NAME.popMessageStream(_token); \
-  } while(0)
+    qtTest::StreamPointer _stream(new qtStringStream); \
+    auto const _token = TEST_OBJECT_NAME.pushMessageStream(_stream); \
+    auto const _test = !!(_expr); \
+    TEST_OBJECT_NAME.test(!_test, "(XFAIL) " #_expr, _QT_TEST_WHERE_); \
+    if(_test) \
+        TEST_OBJECT_NAME.out() << *_stream->string(); \
+    TEST_OBJECT_NAME.popMessageStream(_token); \
+    } while(0)
 
 //-----------------------------------------------------------------------------
-int qtTest::test(bool exprValue, const QString& exprText,
-                 int line, const char* file, const char* func)
+int qtTest::test(bool exprValue, QString const& exprText,
+                 int line, char const* file, char const* func)
 {
-  if (!exprValue)
+    if (!exprValue)
     {
-    qtTestTrace t(*this, line, file, func);
-    this->out() << this->where() << "assertion (" << exprText << ") failed\n";
-    return this->setTestResult(1);
+        qtTestTrace t(*this, line, file, func);
+        this->out() << this->where()
+                    << "assertion (" << exprText << ") failed\n";
+        return this->setTestResult(1);
     }
 
-  return 0;
+    return 0;
 }
 
 //-----------------------------------------------------------------------------
 template <typename T>
-int qtTest::testEqual(T expectedValue, T actualValue, const QString& exprText,
-                      int line, const char* file, const char* func)
+int qtTest::testEqual(T expectedValue, T actualValue, QString const& exprText,
+                      int line, char const* file, char const* func)
 {
-  if (actualValue != expectedValue)
-    return this->failedEqualityTest(expectedValue, actualValue, exprText,
-                                    line, file, func);
-  return 0;
+    if (actualValue != expectedValue)
+    {
+        return this->failedEqualityTest(expectedValue, actualValue, exprText,
+                                        line, file, func);
+    }
+    return 0;
 }
 
 //-----------------------------------------------------------------------------
 template <typename T>
 int qtTest::failedEqualityTest(
-  T expectedValue, T actualValue, const QString& exprText,
-  int line, const char* file, const char* func)
+    T expectedValue, T actualValue, QString const& exprText,
+    int line, char const* file, char const* func)
 {
-  qtTestTrace t(*this, line, file, func);
+    qtTestTrace t(*this, line, file, func);
 
-  QString expectedStr;
-  QDebug dbgExpected(&expectedStr);
-  dbgExpected << expectedValue;
-  expectedStr = expectedStr.trimmed();
+    QString expectedStr;
+    QDebug dbgExpected{&expectedStr};
+    dbgExpected << expectedValue;
+    expectedStr = expectedStr.trimmed();
 
-  QString buffer;
-  QDebug dbg(&buffer);
-  dbg.nospace() << qPrintable(this->where()) << "expression " << exprText;
-  dbg.space() << "gave value" << actualValue << "(expected";
-  dbg.nospace() << qPrintable(expectedStr) << ")\n";
-  this->out() << buffer;
+    QString buffer;
+    QDebug dbg{&buffer};
+    dbg.nospace() << qPrintable(this->where()) << "expression " << exprText;
+    dbg.space() << "gave value" << actualValue << "(expected";
+    dbg.nospace() << qPrintable(expectedStr) << ")\n";
+    this->out() << buffer;
 
-  return this->setTestResult(1);
+    return this->setTestResult(1);
 }
 
 #endif
