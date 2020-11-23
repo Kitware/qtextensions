77f98e1d738ad000229e8d0cc338f6283e6b78cd
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-11-23 10:24:06 -0500

    Update style for `qtComboBoxDelegate`
    Update qtComboBoxDelegate to modern code style and Qt5 pointer-to-member
    slots. Use a queued event to trigger the popup rather than mucking about
    with flag properties.

--- itemviews/qtComboBoxDelegate.cpp
+++ itemviews/qtComboBoxDelegate.cpp
@@ -10,7 +10,7 @@
 
 //-----------------------------------------------------------------------------
 qtComboBoxDelegate::qtComboBoxDelegate(QObject* parent)
-  : qtAbstractListDelegate(parent)
+    : qtAbstractListDelegate(parent)
 {
 }
 
@@ -22,63 +22,58 @@ qtComboBoxDelegate::~qtComboBoxDelegate()
 //-----------------------------------------------------------------------------
 QWidget* qtComboBoxDelegate::createListEditor(QWidget* parent) const
 {
-  // Create combo box
-  QComboBox* box = new QComboBox(parent);
-  box->setFocusPolicy(Qt::StrongFocus);
-  connect(box, SIGNAL(currentIndexChanged(int)),
-          this, SLOT(editorValueChanged()));
+    // Create combo box
+    auto* const box = new QComboBox{parent};
+    box->setFocusPolicy(Qt::StrongFocus);
+    connect(box, QOverload<int>::of(&QComboBox::currentIndexChanged),
+            this, &qtComboBoxDelegate::editorValueChanged);
 
-  // Fill combo box
-  foreach (auto const& name, this->valueNames())
-    box->addItem(name, this->valueData(name));
+    // Fill combo box
+    for (auto const& name : this->valueNames())
+        box->addItem(name, this->valueData(name));
 
-  // Flag box to show pop-up once we know the geometry
-  box->setProperty("firstShow", true);
+    // Queue request for box to show pop-up (needs to be delayed because we
+    // don't know the geometry yet)
+    QMetaObject::invokeMethod(box, &QComboBox::showPopup,
+                              Qt::QueuedConnection);
 
-  // Done
-  return box;
+    // Done
+    return box;
 }
 
 //-----------------------------------------------------------------------------
 void qtComboBoxDelegate::setListEditorData(
-  QWidget* editor, const QVariant& newData) const
+    QWidget* editor, QVariant const& newData) const
 {
-  QComboBox* box = qobject_cast<QComboBox*>(editor);
+    auto* const box = qobject_cast<QComboBox*>(editor);
 
-  int i = -1;
-  foreach (int const j, qtIndexRange(box->count()))
+    auto i = decltype(box->count()){-1};
+    for (auto const j : qtIndexRange(box->count()))
     {
-    if (this->compareData(newData, box->itemData(j)))
-      {
-      i = j;
-      break;
-      }
+        if (this->compareData(newData, box->itemData(j)))
+        {
+            i = j;
+            break;
+        }
     }
-  box->setCurrentIndex(i);
+    box->setCurrentIndex(i);
 }
 
 //-----------------------------------------------------------------------------
 void qtComboBoxDelegate::setModelData(
-  QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const
+    QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const
 {
-  QComboBox* box = qobject_cast<QComboBox*>(editor);
-  QVariant newData = box->itemData(box->currentIndex());
-  this->setModelData(editor, box->currentText(), newData, model, index);
+    auto* const box = qobject_cast<QComboBox*>(editor);
+    auto const& newData = box->itemData(box->currentIndex());
+    this->setModelData(editor, box->currentText(), newData, model, index);
 }
 
 //-----------------------------------------------------------------------------
 void qtComboBoxDelegate::updateEditorGeometry(
-  QWidget* editor,
-  const QStyleOptionViewItem& option,
-  const QModelIndex&) const
+    QWidget* editor,
+    QStyleOptionViewItem const& option,
+    QModelIndex const&) const
 {
-  QComboBox* box = qobject_cast<QComboBox*>(editor);
-  box->setGeometry(option.rect);
-
-  // Show pop-up at creation after the geometry is initialized
-  if (box->property("firstShow").toBool())
-    {
-    box->showPopup();
-    box->setProperty("firstShow", false);
-    }
+    auto* const box = qobject_cast<QComboBox*>(editor);
+    box->setGeometry(option.rect);
 }
--- itemviews/qtComboBoxDelegate.h
+++ itemviews/qtComboBoxDelegate.h
@@ -12,21 +12,23 @@ class QTE_EXPORT qtComboBoxDelegate : public qtAbstractListDelegate
   Q_OBJECT
 
 public:
-  qtComboBoxDelegate(QObject* parent = 0);
-  virtual ~qtComboBoxDelegate();
+    qtComboBoxDelegate(QObject* parent = nullptr);
+    virtual ~qtComboBoxDelegate();
 
-  virtual void setModelData(QWidget* editor, QAbstractItemModel*,
-                            const QModelIndex&) const;
+    virtual void setModelData(
+        QWidget* editor, QAbstractItemModel* model,
+        QModelIndex const& index) const override;
 
-  virtual void updateEditorGeometry(QWidget* editor,
-                                    const QStyleOptionViewItem&,
-                                    const QModelIndex&) const;
+    virtual void updateEditorGeometry(
+        QWidget* editor, QStyleOptionViewItem const& opt,
+        QModelIndex const& index) const override;
 
 protected:
   using qtAbstractListDelegate::setModelData;
 
-  virtual QWidget* createListEditor(QWidget* parent) const;
-  virtual void setListEditorData(QWidget* editor, const QVariant&) const;
+  virtual QWidget* createListEditor(QWidget* parent) const override;
+  virtual void setListEditorData(
+      QWidget* editor, QVariant const& newData) const override;
 
 private:
   QTE_DISABLE_COPY(qtComboBoxDelegate);
